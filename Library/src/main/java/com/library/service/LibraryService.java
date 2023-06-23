3e28021c21a9ddb7ee61ee0de1532bc338b21680
CorazzaJr <82675498+CorazzaJr@users.noreply.github.com>
2023-06-22 21:29:32 -0300

    Add LibraryService

--- Library/src/main/java/com/library/service/LibraryService.java
+++ Library/src/main/java/com/library/service/LibraryService.java
@@ -18,6 +18,14 @@ public class LibraryService {
     public void addBooks(Book book) {
         this.library.getBooks().add(book);
     }
+     
+    public List<Book> searchBooks(String title, Author author, Publisher publisher) {
+        return library.getBooks().stream()
+                .filter(book -> (title == null || book.getTitle().equals(title)) &&
+                                (author == null || book.getAuthor().equals(author)) &&
+                                (publisher == null || book.getPublisher().equals(publisher)))
+                .collect(Collectors.toList());
+    }
 
 }
 
